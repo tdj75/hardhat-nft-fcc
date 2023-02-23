5ac70fb29e02feb19be91433acf49bbea8d7d3bf
tdj75 <taurus.darkbeard@gmail.com>
2023-02-23 21:27:30 +0100

    Basic NFT commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,108 +1,99 @@
-require("@nomiclabs/hardhat-waffle")
-require("hardhat-gas-reporter")
-require("@nomiclabs/hardhat-etherscan")
-require("dotenv").config()
-require("solidity-coverage")
-require("hardhat-deploy")
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-gas-reporter");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
+require("solidity-coverage");
+require("hardhat-deploy");
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 
-const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL
-const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
-const GANACHE_RPC_URL = process.env.GANACHE_RPC_URL
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL;
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
+const GANACHE_RPC_URL = process.env.GANACHE_RPC_URL;
 
-const MNEMONIC_SEED = process.env.MNEMONIC_SEED
+const MNEMONIC_SEED = process.env.MNEMONIC_SEED;
 
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
-const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
 
-const REPORT_GAS = process.env.REPORT_GAS
+const REPORT_GAS = process.env.REPORT_GAS;
 
 module.exports = {
-    defaultNetwork: "hardhat",
-    networks: {
-        hardhat: {
-            chainId: 31337,
-            forking: {
-                url: MAINNET_RPC_URL,
-            },
-            accounts: {
-                mnemonic: MNEMONIC_SEED,
-            },
-        },
-        localhost: {
-            chainId: 31337,
-            forking: {
-                url: MAINNET_RPC_URL,
-            },
-            accounts: {
-                mnemonic: MNEMONIC_SEED,
-            },
-        },
-        ganache: {
-            chainId: 1337,
-            url: GANACHE_RPC_URL,
-            accounts: {
-                mnemonic: MNEMONIC_SEED,
-            },
-        },
-        goerli: {
-            chainId: 5,
-            url: GOERLI_RPC_URL,
-            accounts: {
-                mnemonic: MNEMONIC_SEED,
-            },
-            saveDeployments: true,
-        },
-        mainnet: {
-            chainId: 1,
-            url: MAINNET_RPC_URL,
-            accounts: {
-                mnemonic: MNEMONIC_SEED,
-            },
-            saveDeployments: true,
-        },
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      forking: {
+        url: MAINNET_RPC_URL,
+      },
+      accounts: {
+        mnemonic: MNEMONIC_SEED,
+      },
     },
-    solidity: {
-        compilers: [
-            {
-                version: "0.8.8",
-            },
-            {
-                version: "0.6.12",
-            },
-            {
-                version: "0.4.19",
-            },
-            {
-                version: "0.6.6",
-            },
-        ],
+    localhost: {
+      chainId: 31337,
+      forking: {
+        url: MAINNET_RPC_URL,
+      },
+      accounts: {
+        mnemonic: MNEMONIC_SEED,
+      },
     },
-    etherscan: {
-        apiKey: {
-            goerli: ETHERSCAN_API_KEY,
-            mainnet: ETHERSCAN_API_KEY,
-        },
+    ganache: {
+      chainId: 1337,
+      url: GANACHE_RPC_URL,
+      accounts: {
+        mnemonic: MNEMONIC_SEED,
+      },
     },
-    gasReporter: {
-        enabled: REPORT_GAS,
-        currency: "USD",
-        outputFile: "gas-report.txt",
-        noColors: true,
-        coinmarketcap: COINMARKETCAP_API_KEY,
+    goerli: {
+      chainId: 5,
+      url: GOERLI_RPC_URL,
+      accounts: {
+        mnemonic: MNEMONIC_SEED,
+      },
+      saveDeployments: true,
     },
-    namedAccounts: {
-        deployer: {
-            default: 0,
-        },
-        player: {
-            default: 1,
-        },
+    mainnet: {
+      chainId: 1,
+      url: MAINNET_RPC_URL,
+      accounts: {
+        mnemonic: MNEMONIC_SEED,
+      },
+      saveDeployments: true,
     },
-    mocha: {
-        timeout: 200000, // 200 seconds max for running tests
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.7",
+      },
+    ],
+  },
+  etherscan: {
+    apiKey: {
+      goerli: ETHERSCAN_API_KEY,
+      mainnet: ETHERSCAN_API_KEY,
     },
-}
+  },
+  gasReporter: {
+    enabled: REPORT_GAS,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    coinmarketcap: COINMARKETCAP_API_KEY,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    player: {
+      default: 1,
+    },
+  },
+  mocha: {
+    timeout: 200000, // 200 seconds max for running tests
+  },
+};
